75d1129830c1c2cd2f888b602469182aa76a184c
Ashutosh <83129217+Ashutosh434@users.noreply.github.com>
2021-11-19 11:51:09 +0530

    Delete A3 directory

--- A3/LengthOfString.java
+++
@@ -1,9 +0,0 @@
-package A3;
-public class LengthOfString {
-    public static void main(String[] args) {
-        String str = "Hello World";
-        int l = str.length();
-        System.out.println("length of "+ str + " is " +l);
-    }
-
-}
